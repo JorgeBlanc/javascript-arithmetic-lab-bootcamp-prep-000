9e83d483ab7ef229a60022ca3cdeab5d52955982
George White <thegeorgewilliam@gmail.com>
2018-04-12 17:41:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,17 @@ return (n + 1);
 }
 
 function dec(n) {
+<<<<<<< HEAD
   return (n - 1);
 }
 
 function makeInt(n, 10) {
   return parsrInt(9, 10);
+=======
+  return (n - 1)
+}
+
+function makeInt(n) {
+  return (`n`, 10)
+>>>>>>> 15419067b944e5ad4476b9009fabaf8753631e4f
 }
